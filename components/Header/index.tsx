448b447c1b26b6f89bf5289a367cf98f93fae758
heluansantos <heluan.caetano@gmail.com>
2023-02-10 19:25:08 +0000

    feat: inprovments in header styles

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,59 +1,28 @@
-import { Image } from "react-native";
-import { Text, View } from "../Themed";
-import { Container } from "./styles";
-import IconLogo from '../../assets/icons/IconLogo.svg'
-
+import { View } from "../Themed";
+import { Container, Image, Name, SubTitle, Title, ViewRow } from "./styles";
+import IconLogo from "../../assets/icons/IconLogo.svg";
+import { useState } from "react";
 
 const Header = () => {
+  const [user] = useState({
+    image: "https://reactnative.dev/img/tiny_logo.png",
+    name: "Heluan",
+  });
   return (
     <Container>
-        <View style={{
-          flexDirection: 'row',
-          alignItems: 'center'
-        }}>
-            <View>
-            <IconLogo/>
-            </View>
-           <View>
-            <Text style={{
-              fontSize: 14,
-              lineHeight: 19,
-              marginLeft: 10,
-              color: '#D19DF5',
-            }}>Slide Labs</Text>
-           </View>
+      <ViewRow>
+        <IconLogo />
+        <Title>Slide Labs</Title>
+      </ViewRow>
+      <ViewRow>
+        <Image source={{ uri: user?.image }} />
+        <View>
+          <SubTitle>Hello</SubTitle>
+          <Name>{user?.name}</Name>
         </View>
-        <View style={{
-          flexDirection: 'row',
-          alignItems: 'center'
-        }}>
-          <View>
-            <Image 
-            source={{
-              uri: 'https://reactnative.dev/img/tiny_logo.png',
-            }}
-            style={{
-              height: 30,
-              width: 30,
-              borderRadius: 15,
-              marginRight: 2,
-            }}/>
-          </View>
-         <View>
-          <Text style={{
-              fontSize: 8,
-              lineHeight: 11,
-              color: '#838383',
-            }}>Hello</Text>
-          <Text style={{
-              fontSize: 12,
-              lineHeight: 16,
-              color: '#D3D3D3',
-            }}>Heluan</Text>
-         </View>
-        </View>
+      </ViewRow>
     </Container>
   );
-}
+};
 
 export default Header;
